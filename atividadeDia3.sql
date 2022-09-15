6af45a2213325d83c9f25624246030cc27b9962d
DanielWPitsch <102161140+DanielWPitsch@users.noreply.github.com>
2022-09-15 01:24:33 -0300

    up2

--- atividadeDia3.sql
+++ atividadeDia3.sql
@@ -13,6 +13,27 @@ insert into clientes
 values
 ('eu', '12345678912', 8399112233, 'Avenida esperança, manaira, 3022');
 
+insert into clientes 
+(nome, cpf, telefone, endereco)
+values
+('tu', '12342278912', 8399220022, 'Avenida Poeta, Bessa, 2122');
+
+insert into clientes 
+(nome, cpf, telefone, endereco)
+values
+('ele', '12995678912', 8377112233, 'Avenida Dom Pedro I, Centro, 1122');
+
+insert into clientes 
+(nome, cpf, telefone, endereco)
+values
+('nos', '12345998912', 8399992233, 'Avenida Monteiro da Franca, sei lah, 3332');
+
+insert into clientes 
+(nome, cpf, telefone, endereco)
+values
+('vos', '12388678912', 8390012233, 'Avenida Santa Catarina, Bairro dos estados, 8976');
+
+
 create table produtos(
 nome varchar(10),
 codigo int(10),
@@ -23,3 +44,23 @@ insert into produtos
 (nome, codigo, preco)
 values
 ('pao', 123, 000.70);
+
+insert into produtos 
+(nome, codigo, preco)
+values
+('bolo', 122, 007.70);
+
+insert into produtos 
+(nome, codigo, preco)
+values
+('leite', 121, 010.70);
+
+insert into produtos 
+(nome, codigo, preco)
+values
+('queijo', 120, 043.70);
+
+insert into produtos 
+(nome, codigo, preco)
+values
+('café', 111, 002.70);
